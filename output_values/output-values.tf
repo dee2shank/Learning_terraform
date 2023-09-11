6dca4e40e20bdf7e6dd60f884016aeb31561ef73
Deepu shankar kv <deepu.shankar@outlook.com>
2023-09-11 11:58:03 +0530

    first commit

---
+++ output_values/output-values.tf
@@ -0,0 +1,13 @@
+#define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_eip" "lb1" {
+    domain   = "vpc"
+}
+
+output "public-ip" {
+  value = aws_eip.lb1.public_ip
+}
